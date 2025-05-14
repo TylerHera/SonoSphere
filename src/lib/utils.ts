948f5db3faa7b1beb97c99af8134d6ee3962ace8
Tyler Hera <tyler.okello@gmail.com>
2025-05-15 02:17:12 +0300

    Phase 1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const placeholderImage = (width: number, height: number, text: string = "No Image") => {
+  return `https://via.placeholder.com/${width}x${height}.png?text=${encodeURIComponent(text)}`;
+};
