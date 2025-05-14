02fd38e079a79dcf3eb408c672824fcd4de5f36e
Tyler Hera <tyler.okello@gmail.com>
2025-05-15 02:26:58 +0300

    Phase 1

--- src/components/providers/AuthProvider.tsx
+++ src/components/providers/AuthProvider.tsx
@@ -43,7 +43,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     );
 
     return () => {
-      authListener?.unsubscribe();
+      authListener?.subscription.unsubscribe();
     };
   }, [supabase]);
 
